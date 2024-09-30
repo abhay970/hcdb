d938d8bf5d151b08e39ec5786632d22d5eafc2b1
Abhay Yadav <119952759+abhay970@users.noreply.github.com>
2024-09-30 20:18:28 +0530

    Add files via upload

---
+++ hc-main/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+// import ComponentTest from "./predevelopments/ComponentTest";
+import Base from "./components/Base";
+// import SearchQuestions from "./components/SearchQuestions";
+
+function App() {
+  return (
+    <div className="App">
+      <Base />
+      {/* <ComponentTest /> */}
+      {/* <SearchQuestions /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ hc-main/src/predevelopments/ComponentTest.js
@@ -0,0 +1,7 @@
+import "./ComponentTest.css";
+
+const ComponentTest = () => {
+  return <div className="ComponentTest">deb</div>;
+};
+
+export default ComponentTest;
